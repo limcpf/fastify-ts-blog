c3ec0063654bc7eb3a889048d774ff452d20d103
limcpf <matathresh1740@naver.com>
2022-12-04 22:34:53 +0900

    feat : add cors, jwt setting feat : add rome for format, lint in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,36 @@
-import Fastify, {FastifyInstance} from 'fastify';
-import * as dotenv from 'dotenv';
-import {routes} from './config/router.config';
+import Fastify, { FastifyInstance } from "fastify";
+import * as dotenv from "dotenv";
+import { routes } from "./config/router.config";
+import fastifyCors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 
 /* environment variable setting */
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 /* fastify instance setting */
-const fastify:FastifyInstance = Fastify({
-    logger: process.env.NODE_ENV !== "production"
+const fastify: FastifyInstance = Fastify({
+	logger: process.env.NODE_ENV !== "production",
 });
-
-
 
 /* router setting */
 fastify.register(routes);
 
+/* cors setting */
+fastify.register(fastifyCors, {
+	origin: true,
+	credentials: true,
+});
 
+/* jwt setting */
+const secret = process.env.JWT_TOKEN_SECRET;
+if(!secret) process.exit(1);
+fastify.register(jwt, { secret });
 
 /* server running */
 fastify.listen({ port: 3000 }, function (err) {
-    if (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
-    fastify.log.info(`NODE_ENV is ${process.env.NODE_ENV}`);
+	if (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+	fastify.log.info(`NODE_ENV is ${process.env.NODE_ENV}`);
 });
