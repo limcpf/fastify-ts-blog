083767593dff677d7f467c27bfacb6255ea071a1
limcpf <matathresh1740@naver.com>
2022-12-04 22:35:56 +0900

    feat: add time.class.ts feat: add user schema

--- src/domain/time.class.ts
+++ src/domain/time.class.ts
@@ -1,9 +1,9 @@
 export class TimeClass {
-    readonly createdAt: string;
-    readonly updatedAt: string;
+	readonly createdAt: string;
+	readonly updatedAt: string;
 
-    constructor(createAt:string, updateAt:string) {
-        this.createdAt = createAt;
-        this.updatedAt = updateAt;
-    }
+	constructor(createAt: string, updateAt: string) {
+		this.createdAt = createAt;
+		this.updatedAt = updateAt;
+	}
 }
